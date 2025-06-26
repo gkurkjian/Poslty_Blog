f69c349801d7f4dec8d04174f0d51328e2ec9a1c
George K <gkurkjian@myseneca.ca>
2025-06-26 16:36:14 -0400

    eight commit:  created basic about page

---
+++ app/about/page.js
@@ -0,0 +1,8 @@
+export default function aboutUs() {
+    return (
+        <main style={{ padding: '2rem' }}>
+            <h1>About Us</h1>
+            <p>This is a web/app blog posts with it will display by its content, image, date and the author.</p>
+        </main>
+    );
+}
