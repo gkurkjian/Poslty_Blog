6a679ed491fbf9faeaf42549dc957db2b32fe2d6
George K <gkurkjian@myseneca.ca>
2025-07-12 19:53:54 -0400

    twentieth-second commit:  update CategoryPage to use consistent parameter naming and improve code readability

--- app/category/[category]/page.js
+++ app/category/[category]/page.js
@@ -1,16 +1,17 @@
 import BlogCard from "@/components/BlogCard";
+import blogData from "@/data/blogData.json";
 
-export default function CategoryPage ({ param}) {
-    const posts = blogData.filter(post => post.category === param);
+export default function CategoryPage({ params }) {
+  const posts = blogData.filter(post => post.category === params.category);
 
-    return (
+  return (
     <main className="container py-5">
       <h1 className="mb-4">Category: {params.category}</h1>
       <div className="row g-4">
         {posts.map(post => (
-            <div key={post.id} className="col-12 col-sm-6 col-lg-4">
-                <BlogCard post={post} showButton={true} showContent={false} />
-            </div>
+          <div key={post.id} className="col-12 col-sm-6 col-lg-4">
+            <BlogCard post={post} showButton={true} showContent={false} />
+          </div>
         ))}
       </div>
     </main>
